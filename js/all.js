7a2d50bf9fa88f786582f7eab502e9f3276d2f69
bbyoulike5487 <bbyoulike5487@gmail.com>
2025-03-18 17:05:16 +0800

    code prettier

--- js/all.js
+++ js/all.js
@@ -1,17 +1,17 @@
 function isIOS() {
   return /iPad|iPhone|iPod/.test(navigator.userAgent);
-};
+}
 
 // banner bg 在 ios 系統上的視差滾動效果
-if(isIOS()) {
-  const bannerImage = document.querySelector(".banner-image");
+if (isIOS()) {
+  const bannerImage = document.querySelector('.banner-image');
   bannerImage.style.cssText = `
     transform: translateY(0);
     transition: transform 0.001s ease-out;
   `;
-  bannerImage.classList.remove("background-attachment");
+  bannerImage.classList.remove('background-attachment');
 
-  window.addEventListener("scroll", function () {
+  window.addEventListener('scroll', function () {
     let scrollY = window.scrollY;
     bannerImage.style.transform = `translateY(${scrollY * 0.3}px)`;
   });
